21dacb76b6f2bbb62aeb5a57e181d67550a9fb48
Codelearner-creator <albjanmehmetaj111@gmail.com>
2026-04-16 12:49:46 +0200

    Update shadow_merge.py
    Simplified mergeList function to directly merge and sort both lists.

--- py_shadow_merge/shadow_merge.py
+++ py_shadow_merge/shadow_merge.py
@@ -1,16 +1,5 @@
-from typing import List
 def mergeList(list1: List[int], list2: List[int]):
-    if list1 is None:
-        return sorted(list2)
-    if list2 is None:
-        return sorted(list1)
-
-    final_list = []
-    for i in list2:
-        final_list.append(i)
-    for j in list1:
-        final_list.append(j)
-    return sorted(final_list)
+    return sorted(list1+list2)
 
 
 print(mergeList(None, [0, 8, 2, 1]))
